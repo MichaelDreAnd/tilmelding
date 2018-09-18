7ce3791d4e0a82d3c2f29df7d045298b68435069
Michael Drejer Andersen <mich450m@student.eadania.dk>
2018-09-18 11:45:58 +0200

    Second commit

---
+++ MichaelOgNicklasMiniProjekt/MichaelOgNicklasMiniProjekt/App_Code/createmodel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for createmodel
+/// </summary>
+public class createmodel
+{
+    public createmodel()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
